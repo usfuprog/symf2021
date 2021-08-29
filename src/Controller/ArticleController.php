9b9e5005d99351e10f62326a65e4743c81e9397c
nuwb <arplex@yahoo.com>
2021-08-29 18:34:47 +0200

    [20210829]->lesson 1, video 3, complete

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,14 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
-
+    /**
+     * @Route("/")
+     */
     public function rentflatpage()
     {
         return new Response('rent flat page');
     }
+
+    /**
+     * @Route("/articles/{slug}")
+     */
+    public function show($slug)
+    {
+       return new Response(sprintf(
+           "future content about: %s", 
+           ucwords(str_replace('-', ' ', $slug))));
+    }
+
 }
 
 ?>
